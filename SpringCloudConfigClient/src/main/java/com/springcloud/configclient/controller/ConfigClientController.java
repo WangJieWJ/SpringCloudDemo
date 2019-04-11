5cb18345338456ff0813dfff445e26e0604d4580
wangjie <1152689483@qq.com>
2019-04-11 21:43:23 +0800

    更换config中的key

--- SpringCloudConfigClient/src/main/java/com/springcloud/configclient/controller/ConfigClientController.java
+++ SpringCloudConfigClient/src/main/java/com/springcloud/configclient/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.springcloud.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("client")
 public class ConfigClientController {
 
-    @Value("${requestURL}")
+    @Value("${request.url}")
     private String requestURL;
 
-
+    @GetMapping("getConfigValue")
     public String getConfigValue() {
         return "获取到的requestURL为:" + requestURL;
     }
