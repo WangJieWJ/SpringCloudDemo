9b1713814d351538d1b5694d36c56d7e3777467c
wangjie <1152689483@qq.com>
2019-04-11 23:09:25 +0800

    测试SpringCloudConfigClient的配置信息热加载，集成spring-boot-starter-actuator，用POST方式调用http://localhost:8004/spring-cloud-config-client/refresh

--- SpringCloudConfigClient/src/main/java/com/springcloud/configclient/controller/ConfigClientController.java
+++ SpringCloudConfigClient/src/main/java/com/springcloud/configclient/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.springcloud.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("client")
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class ConfigClientController {
 
     @Value("${request.url}")
